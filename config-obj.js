96054005353470b923771605f8fb6cb18dac91df
dd552xx3-hub <dd552xx3@gmail.com>
2026-05-11 13:51:06 +0700

    Create config-obj.js

---
+++ config-obj.js
@@ -0,0 +1,8 @@
+const globalFirebaseConfig = {
+    apiKey: "AIzaSyDBENGuoNVCBveOfFnGwh2CkhX9ckoB5Gk",
+    authDomain: "doantn50cntt.firebaseapp.com",
+    projectId: "doantn50cntt",
+    storageBucket: "doantn50cntt.firebasestorage.app",
+    messagingSenderId: "705512357448",
+    appId: "1:705512357448:web:844e8f876c489ae27d"
+};
